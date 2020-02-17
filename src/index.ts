7da22cc5e8c15f873429a307086950bc53459aa1
unknown <stefaleon@gmail.com>
2020-02-17 14:40:34 +0200

    configure the User setter and make the properties optional

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,13 @@ const user = new User({ name: "maimous", age: 666 });
 
 console.log(user.get("name"));
 console.log(user.get("age"));
+
+user.set({ name: "maimoudaros", age: 999 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
+
+user.set({ age: 123 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
