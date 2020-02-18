d60dc2286683bc1549da43ba29faa2ec4a5675ca
stefaleon <stefaleon@gmail.com>
2020-02-18 09:48:43 +0200

    configure the trigger method which runs the callbacks of user events

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,15 @@ const user = new User({ name: "maimous", age: 666 });
 user.on("change", () => {
   console.log("something changed for user");
 });
-user.on("change", () => {}); // another callback for the change event
-user.on("dance", () => {}); // the dance event
+user.on("change", () => {
+  console.log("something else changed for user");
+}); // another callback for the change event
+user.on("dance", () => {
+  console.log("user is in a dancing event");
+}); // the dance event
 
 console.log(user);
+user.trigger("change");
+user.trigger("dance");
+user.trigger("an invalid event");
+user.trigger("change");
