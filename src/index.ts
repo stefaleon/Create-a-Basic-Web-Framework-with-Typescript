251996be2814b54e6116c657780cf6639df19e55
stefaleon <stefaleon@gmail.com>
2020-02-19 11:01:10 +0200

    refactor Sync to a generic class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const BACK_END_URL = "http://localhost:4444/users";
-
-const user = new User({}, BACK_END_URL);
+const user = new User({});
 
 const data = { name: "maimous", age: 666 };
-user.sync.save(data);
-user.sync.fetch(1).then(res => console.log(res));
+user.sync.save(data).then(res => console.log("sync save result: ", res));
+user.sync
+  .fetch(1)
+  .then(res => console.log("sync fetch result for id=1: ", res));
--- src/models/User.ts
+++ src/models/User.ts
@@ -7,12 +7,14 @@ export interface UserProps {
   age?: number;
 }
 
+const BACK_END_URL = "http://localhost:4444/users";
+
 export class User {
   events: Events = new Events();
 
-  sync: Sync = new Sync(this.url);
+  sync: Sync<UserProps> = new Sync(BACK_END_URL);
 
-  constructor(private data: UserProps, private url: string) {}
+  constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
     return this.data[propName];
