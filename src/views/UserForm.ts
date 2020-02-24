2dbf143b1a410e1a22028be229b6dfa8aaf2dc83
stefaleon <stefaleon@gmail.com>
2020-02-24 09:26:49 +0200

    bind event handlers

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,16 +1,46 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      "click:button": this.onButtonClick
+    };
+  }
+
+  onButtonClick(): void {
+    console.log("button clicked!");
+  }
+
   template(): string {
     return `<div>
     <h1>User Form</h1>
     <input />
+    <button>Click me</button>
     </div>`;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      console.log(eventName);
+      console.log(selector);
+
+      fragment
+        .querySelectorAll(selector)
+        .forEach(element =>
+          element.addEventListener(eventName, eventsMap[eventKey])
+        );
+    }
   }
 
   render(): void {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
