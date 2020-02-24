63532f5b9f8c5677155f13bcf10f1ed165f83b92
stefaleon <stefaleon@gmail.com>
2020-02-24 16:19:12 +0200

    extract the reusable part from UserForm to the abstract class View

---
+++ src/views/View.ts
@@ -0,0 +1,44 @@
+import { User } from "../models/User";
+
+export abstract class View {
+  constructor(public parent: Element, public model: User) {
+    this.renderOnChange();
+  }
+
+  renderOnChange(): void {
+    this.model.on("change", () => {
+      this.render();
+    });
+  }
+
+  abstract eventsMap(): { [key: string]: () => void };
+  abstract template(): string;
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      // console.log(eventName);
+      // console.log(selector);
+
+      fragment
+        .querySelectorAll(selector)
+        .forEach(element =>
+          element.addEventListener(eventName, eventsMap[eventKey])
+        );
+    }
+  }
+
+  render(): void {
+    this.parent.innerHTML = ""; //clear all before each new render
+
+    const templateElement = document.createElement("template");
+    templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
+
+    this.parent.append(templateElement.content);
+  }
+}
